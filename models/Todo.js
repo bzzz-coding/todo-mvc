4d9e7b99681096c3e4d1116e6702463b61e9eede
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2022-08-23 13:13:39 -0700

    Added comments

--- models/Todo.js
+++ models/Todo.js
@@ -1,6 +1,8 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose') // Referencing mongoose
 
+// Define the Schema by constructing a mongoose instance where the key name corresponds to the property name in the collection
 const TodoSchema = new mongoose.Schema({
+  // Define a property called 'todo' with a schema type String which maps to an internal validator that will be triggered when the model is saved to the database. It will fail if the value is not a string.
   todo: {
     type: String,
     required: true,
@@ -11,4 +13,5 @@ const TodoSchema = new mongoose.Schema({
   }
 })
 
+// Exporting the module by calling the model constructor on the Mongoose instance and pass it the name of the collection and a reference to the schema definition.
 module.exports = mongoose.model('Todo', TodoSchema)
--- server.js
+++ server.js
@@ -1,19 +1,25 @@
-const express = require('express')
-const app = express()
-const connectDB = require('./config/database')
+const express = require('express') // require express
+const app = express() // renaming the function for brevity
+
+// connectDB is an async function that connects to the database
+const connectDB = require('./config/database') 
 const homeRoutes = require('./routes/home')
 const todoRoutes = require('./routes/todos')
 
-require('dotenv').config({path: './config/.env'})
+require('dotenv').config({path: './config/.env'}) // require dotenv
 
 connectDB()
 
+// setting the view through ejs
 app.set('view engine', 'ejs')
+// executing middleware to return static files coming from 'public' folder or will return HTTP 404
 app.use(express.static('public'))
+// parses incoming requests with urlencoded payloads
 app.use(express.urlencoded({ extended: true }))
+// convert request body to a JSON Object
 app.use(express.json())
 
-app.use('/', homeRoutes)
+app.use('/', homeRoutes) 
 app.use('/todos', todoRoutes)
  
 app.listen(process.env.PORT, ()=>{
